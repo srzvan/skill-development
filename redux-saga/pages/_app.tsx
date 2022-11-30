50733b09c7f0b4be893e1b9d96df535303791039
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-11-30 14:28:25 +0200

    tsconfig - add paths

--- redux-saga/pages/_app.tsx
+++ redux-saga/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { Provider as StoreProvider } from "react-redux";
 
-import { store } from "../store";
+import { store } from "store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
