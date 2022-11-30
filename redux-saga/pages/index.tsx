198e2afa20b3af3179619091db6cf5e10a8053ce
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-11-30 11:24:39 +0200

    redux-saga - bootstrap nextjs app

---
+++ redux-saga/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <h1>
+      <i>
+        Learn <code>redux-saga</code>
+      </i>{" "}
+      they said. <i>It&aposs gonna be fun</i> they said.
+    </h1>
+  );
+}
