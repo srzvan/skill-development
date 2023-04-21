3d09070563916ce3b536a9537352a3b8862cb5d9
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2023-04-21 09:59:12 +0300

    add start script in package.json

--- backend/nodejs/guides/with-typescript/is-adult.js
+++ backend/nodejs/guides/with-typescript/is-adult.js
@@ -1,8 +1,9 @@
 var justine = {
-    name: "Justine",
-    age: 23,
+  name: "Justine",
+  age: 15,
 };
 function isAdult(user) {
-    return user.age >= 18;
+  return user.age >= 18;
 }
+
 console.log("Is Justine an adult? ".concat(isAdult(justine) ? "Of course she is." : "Definitely not!"));
