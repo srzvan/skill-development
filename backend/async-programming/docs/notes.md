8e55291153d5668006adefee9cf47236bdac8223
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2023-04-26 09:59:21 +0300

    more promises

--- backend/async-programming/docs/notes.md
+++ backend/async-programming/docs/notes.md
@@ -1,3 +1,15 @@
 # Asynchronous programming
 
 ## Promises
+
+### Terminology
+
+## A promise can be in one of 3 states:
+
+- **pending**: the promise has been created, and the asynchronous function it's associated with has not succeeded or failed yet. This is the state your promise is in when it's returned from a call to fetch(), and the request is still being made
+- **fulfilled**: the asynchronous function has succeeded. When a promise is fulfilled, its then() handler is called
+- **rejected**: the asynchronous function has failed. When a promise is rejected, its catch() handler is called
+
+The term **settled** can be used to cover both fulfilled and rejected.
+
+Note that what "succeeded" or "failed" means here is up to the API in question: for example, `fetch()` considers a request successful if the server returned an error like **404 Not Found**, but not if a network error prevented the request being sent.
--- backend/async-programming/src/promise-chaining.js
+++ backend/async-programming/src/promise-chaining.js
@@ -1,14 +1,29 @@
 import fetch from "node-fetch";
 
-console.log("Before calling `fetch`");
+function fetchProducts(withError = false) {
+  const URL = withError
+    ? "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    : "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
 
-fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json")
-  .then(response => {
-    console.log("Data fetched...");
-    return response.json();
-  })
-  .then(data => {
-    console.log(`The name of the first product in the response is "${data[0].name}"`);
-  });
+  console.log("Before calling `fetch`");
 
-console.log("Fetching data...");
+  fetch(URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      console.log("Data fetched");
+      return response.json();
+    })
+    .then(data => {
+      console.log(`There are ${data.length} products & the name of the first product is "${data[0].name}"`);
+    })
+    .catch(error => {
+      console.error(`Could not get products: ${error}`);
+    });
+
+  console.log("Fetching data...");
+}
+
+fetchProducts();
